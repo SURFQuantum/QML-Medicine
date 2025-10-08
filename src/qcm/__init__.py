76f9e4e073f47abed2635e420c9ef576a30b5ac9
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2025-10-08 18:33:09 +0200

    applied bare minimum template

---
+++ src/qcm/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about qcm."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "SURF Quantum"
+__email__ = "nicolas.renaud@surf.nl"
+__version__ = "0.1.0"
