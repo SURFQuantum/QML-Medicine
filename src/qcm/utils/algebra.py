08073dfb8e336dcac434f4818e3bba74190b49a7
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2025-10-10 09:49:28 +0200

    fix tests

--- src/qcm/utils/algebra.py
+++ src/qcm/utils/algebra.py
@@ -6,7 +6,7 @@ def hermitianize_image_matrix(image_matrix: np.ndarray) -> np.ndarray:
     """Make an arbitrary square matrix Hermitian (H = (M + M^†)/2)."""
     return 0.5 * (image_matrix + image_matrix.conj().T)
 
-def compute_density_matrix(state):
+def compute_density_matrix(state: torch.Tensor) -> torch.Tensor:
     """Calculates the density matrix representation of a state.
 
     Args:
